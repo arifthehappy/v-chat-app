73e482e1bbe8109f7e0e38d82861db26fa260068
MD ARIF KHAN <55360458+arifthehappy@users.noreply.github.com>
2022-07-27 20:00:37 +0530

    initialize firebase app

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCwPwa0WgkxdL6GE_g2WmUN0XHjDVwlKtQ',
+  authDomain: 'v-chat-app-b706d.firebaseapp.com',
+  projectId: 'v-chat-app-b706d',
+  storageBucket: 'v-chat-app-b706d.appspot.com',
+  messagingSenderId: '550520261261',
+  appId: '1:550520261261:web:8a05ff8bcd9796436ff0c4',
+};
+
+const app = firebase.initializeApp(config);
