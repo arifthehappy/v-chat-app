bd4068e26eaa18ab9dadc7f9d4d8c803aed15309
MD ARIF KHAN <55360458+arifthehappy@users.noreply.github.com>
2022-07-28 14:06:59 +0530

    added signin page and sign in with google/facebook

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import { Container, Grid, Row, Panel, Col, Button, Icon, Alert } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 
 const SignIn = () => {
-  return <div>SignIn njnj</div>;
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+          email: user.email,
+        });
+      }
+      Alert.success('Signed In', 4000);
+    } catch (err) {
+      Alert.error(err.message, 5000);
+    }
+  };
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to VChat</h2>
+                <p>Progressive chat platform for neophytes</p>
+              </div>
+              <div className="mt-3">
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" className="mr-1" />
+                  Continue with Facebook
+                </Button>
+
+                <Button block color="red" onClick={onGoogleSignIn}>
+                  <Icon icon="google" className="mr-1" />
+                  Continue with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
