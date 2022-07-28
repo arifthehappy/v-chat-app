d1bdbf4e0bce148ee0c359d6e339ac467d5f0b87
MD ARIF KHAN <55360458+arifthehappy@users.noreply.github.com>
2022-07-28 21:52:11 +0530

    added sign out function

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { Button, Icon, Drawer } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Button, Icon, Drawer, Alert } from 'rsuite';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 import Dashboard from '.';
+import { auth } from '../../misc/firebase';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
   const isDesktop = useMediaQuery('(min-width: 992px)');
+
+  const onSignOut = useCallback(() => {
+    try {
+      auth.signOut();
+      Alert.info('You have signed out successfully.', 4000);
+      close();
+    } catch (error) {
+      Alert.error(error.message);
+    }
+  }, [close]);
   return (
     <>
       <Button block color="blue" onClick={open}>
@@ -13,7 +24,7 @@ const DashboardToggle = () => {
         Dashboard
       </Button>
       <Drawer full={!isDesktop} show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
